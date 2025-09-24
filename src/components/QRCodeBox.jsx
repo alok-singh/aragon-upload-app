d3eddc4880eb2e26eb299ef4ee21e99e2874b7e0
Alok <alok_singh1@astro.com.my>
2025-09-24 13:39:32 +0800

    fix: removing unused files

--- src/components/QRCodeBox.jsx
+++
@@ -1,5 +0,0 @@
-const QRCodeBox = () => {
-  return <div className="qr-code-box">QR</div>;
-};
-
-export default QRCodeBox;
--- src/icons/QRIcon.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NoRevealingClothesIcon = ({ className }) => (
-  <div className="qr-icon"></div>
-);
-
-export default NoRevealingClothesIcon;
